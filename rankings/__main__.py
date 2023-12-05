ea3ab15f544370844e083c404791376ace83872c
Michael Knott <michaelknott@live.co.uk>
2023-12-05 20:42:06 +0000

    refactor: add logging to scraper
    This change adds logging to provide monitoring of the scraper when
    deploy.

--- rankings/__main__.py
+++ rankings/__main__.py
@@ -1,14 +1,26 @@
+import logging
 import time
 
 from scraper import get_issf_ranking_html, parse_issf_ranking_html, EVENT_CODES
 from db import Base, Rankings, Session, engine
 
 
+logging.basicConfig(
+    filename="scraper.log",
+    level=logging.INFO,
+    format="%(asctime)s: %(name)s - %(levelname)s - %(message)s "
+    "(Filename: %(filename)s  Line: %(lineno)d  Function: %(funcName)s)",
+    datefmt="%m/%d/%Y %I:%M:%S %p",
+)
+
+
 def main():
     """Main entry point for the scraper."""
+    logging.info("Starting ISSF World Rankings scraper")
     Base.metadata.create_all(bind=engine)
 
     for event_code in EVENT_CODES:
+        logging.info(f"Getting world rankings for event code: {event_code}")
         event, soup = get_issf_ranking_html(event_code)
         rankings = parse_issf_ranking_html(event, soup)
 
@@ -16,7 +28,9 @@ def main():
             with session.begin():
                 for ranking in rankings:
                     session.add(Rankings(**ranking))
+                    logging.info(f"Added world rankings for {event_code} to database")
         time.sleep(0.5)
+    logging.info("ISSF World Rankings scraper finished")
 
 
 if __name__ == "__main__":
